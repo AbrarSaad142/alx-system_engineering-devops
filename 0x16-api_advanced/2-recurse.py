12ac145333513e1dac2b90364dff5a4cefd2c379
Abrar Saad <abrarsaad3572022@gmail.com>
2024-06-04 22:24:50 +0300

    2. Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,13 +7,16 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """Returns a list of titles of all hot articles for a given subreddit recursively."""
+    """Returns a list of titles of all hot articles for
+      a given subreddit recursively."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "python:subreddit.hot.articles:v1.0 (by /u/yourusername)"}
+    headers = {"User-Agent":
+               "python:subreddit.hot.articles:v1.0 (by /u/yourusername)"}
     params = {"after": after}
 
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             articles = data.get('data', {}).get('children', [])
