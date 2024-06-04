3a90e3c9cc5c4814138d962a9e7fa87f8096a413
Abrar Saad <abrarsaad3572022@gmail.com>
2024-06-04 22:21:44 +0300

    2. Recurse it!

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ import requests
 
 
 def top_ten(subreddit):
-    """returns the number of subscribers"""
+    """prints the titles of the first 10 hot posts listed """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     params = {"limit": 10}
