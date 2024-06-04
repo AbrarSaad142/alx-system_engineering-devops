c11a1bb29bc2b01278618705f7dca700314c3ddd
Abrar Saad <abrarsaad3572022@gmail.com>
2024-06-04 22:26:48 +0300

    1. Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,14 +7,25 @@ import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed """
+    """Prints the titles of the first 10 hot posts
+      listed for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    headers = {"User-Agent":
+               "python:subreddit.top.ten:v1.0 (by /u/yourusername)"}
     params = {"limit": 10}
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+
+    try:
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            articles = data.get('data', {}).get('children', [])
+            if not articles:
+                print(None)
+                return
+            for article in articles:
+                print(article['data']['title'])
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
